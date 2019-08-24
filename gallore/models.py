fef89f4a54050b1a22e04f1c05b0de70aa5ed8a3
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-24 19:24:50 +0300

    added the three tables to the database

--- gallore/models.py
+++ gallore/models.py
@@ -9,14 +9,43 @@ class Category(models.Model):
 
 class Location(models.Model):
     name =  models.CharField(max_length =60)
+    def __str__(self):
+        return self.name
 
 class Image(models.Model):
     name = models.CharField(max_length =60)
     description = models.TextField()
     category = models.ManyToManyField(Category)
-    # category = models.ForeignKey(Category,on_delete = models.DO_NOTHING)
     location = models.ForeignKey(Location,on_delete = models.DO_NOTHING)
     upload_date = models.DateTimeField(auto_now_add=True,blank =True)
     image = models.ImageField(upload_to= 'gallery/',default="image")
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
+    
+    # def delete_image(self):
+    #     self.save()
+    
+    # def update_image(self):
+    #     self.save()
+    
+    # def get_image_by_id(id):
+    #     self.save()
+    
+    @classmethod
+    def search_image(cls,category):
+        searchedImage=cls.objects.filter(name__icontains=category)
+        return searchedImage
+    
+    @classmethod
+    def filter_by_location(cls,location):
+        location = cls.objects.filter(location=name)
+        return location
 
 
