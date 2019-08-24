c3f01817b5f72383694ea691c059e892d2b62a12
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-24 21:55:57 +0300

    homepage working perfectly

--- gallore/urls.py
+++ gallore/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
     url(r'^image/(\d+)',views.image,name ='image')
 ]
+# if settings.DEBUG:
+#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
