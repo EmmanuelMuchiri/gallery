d1b2721d9ead5ee6144b639eb18470408560286e
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-24 19:09:47 +0300

    reorganized the structure

--- gallore/models.py
+++ gallore/models.py
@@ -1,18 +1,22 @@
 from django.db import models
 import datetime as dt
 # Create your models here.
-class Article(models.Model):
+
+class Category(models.Model):
+    name =  models.CharField(max_length =60)
+    def __str__(self):
+        return self.name
+
+class Location(models.Model):
+    name =  models.CharField(max_length =60)
+
+class Image(models.Model):
     name = models.CharField(max_length =60)
     description = models.TextField()
-    tags = models.ManyToManyField(tags)
-    category = models.ForeignKey(Category,on_delete = models.DO_NOTHING)
+    category = models.ManyToManyField(Category)
+    # category = models.ForeignKey(Category,on_delete = models.DO_NOTHING)
     location = models.ForeignKey(Location,on_delete = models.DO_NOTHING)
     upload_date = models.DateTimeField(auto_now_add=True,blank =True)
     image = models.ImageField(upload_to= 'gallery/',default="image")
 
-class Category(models.Model):
-    name =  models.CharField(max_length =60)
 
-
-class Location(models.Model):
-    name =  models.CharField(max_length =60)
