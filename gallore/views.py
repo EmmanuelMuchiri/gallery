5ed5949f9038063f7487ad9806ddc28b7596366e
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-24 21:01:16 +0300

    created function image and the respective render templates

--- gallore/views.py
+++ gallore/views.py
@@ -1,5 +1,14 @@
-from django.http  import HttpResponse
-
+import datetime as dt
+from django.http import HttpResponse,Http404
+from django.shortcuts import render,redirect
+from .models import Image
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to the Art Galla')
+
+def image(request,image_id):
+    try:
+        image = Image.objects.get(id = image_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"pics/pics.html", {"image":image})
