55b3b9386e5233bfa6835a3e606c654e33e4c676
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-24 18:50:50 +0300

    created three classes Image,Category and Location and defined their relationships

--- gallore/models.py
+++ gallore/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+import datetime as dt
 # Create your models here.
+class Article(models.Model):
+    name = models.CharField(max_length =60)
+    description = models.TextField()
+    tags = models.ManyToManyField(tags)
+    category = models.ForeignKey(Category,on_delete = models.DO_NOTHING)
+    location = models.ForeignKey(Location,on_delete = models.DO_NOTHING)
+    upload_date = models.DateTimeField(auto_now_add=True,blank =True)
+    image = models.ImageField(upload_to= 'gallery/',default="image")
+
+class Category(models.Model):
+    name =  models.CharField(max_length =60)
+
+
+class Location(models.Model):
+    name =  models.CharField(max_length =60)
