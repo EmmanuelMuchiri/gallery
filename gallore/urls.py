c2601e133113100f59630765995b4563491df73c
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-25 08:17:46 +0300

    confirmed that the tests are working

--- gallore/urls.py
+++ gallore/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import url
+from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views
 
 urlpatterns=[
-    # url('^$',views.welcome,name = 'welcome'),
-    # url('^$',views.name ='image')
+    url('^$',views.main_gallery,name = 'main_gallery'),
+    url(r'^location/(\d+)',views.location,name = 'location'),
+    url(r'^search/',views.search,name='search')
 ]
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
