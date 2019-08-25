bba45ef270eb86f132dc8009a5892f774c46f430
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-25 08:59:00 +0300

    created the static directory and index.css where I will define all my custom css in it

--- gallore/views.py
+++ gallore/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse,Http404
-from .models import Image,Location
+from .models import Image,Location,categories
 
 # Create your views here.
 def main_gallery(request):
@@ -18,4 +18,4 @@ def search(request):
     if 'category' in request.GET and request.GET["category"]:
         search_term = request.GET.get("category")
         searched_images = Image.search_by_category(search_term)
-    return render(request,'search.html',{"images":searched_images,"category":search_term})
+        return render(request,'search.html',{"images":searched_images,"category":search_term})
